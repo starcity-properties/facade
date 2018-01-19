367bd32c550395794111df26240e86558851a47d
Josh Lehman <jalehman37@gmail.com>
2018-01-18 18:23:46 -0800

    Version 0.1.10-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/facade "0.1.9"
+(defproject starcity/facade "0.1.10-SNAPSHOT"
   :description "Provides HTML templates and snippets for Starcity's front-end."
   :url "https://github.com/starcity-properties/facade"
   :license {:name "Eclipse Public License"
